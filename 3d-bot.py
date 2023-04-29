71fd5379da2fa02cef190c36ff377529370af2c5
klausklausen <florian@florian-grassmann.de>
2023-04-29 12:35:17 +0200

    add mqtt client

--- 3d-bot.py
+++ 3d-bot.py
@@ -9,6 +9,7 @@ from octopi import OctoPi
 from job import Job
 from logger import Logger
 from telegram_client import Telegram
+from mqtt_client import MQTT
 
 logger = telegram = octopi = job = None
 
@@ -18,15 +19,22 @@ class Runtime():
     logger = Logger(debug=debug)
     telegram = Telegram(debug=debug)
     octopi = OctoPi(debug=debug)
+    mqtt = MQTT(debug=debug)
     job = None
     if not debug:
         logging.getLogger('asyncio').setLevel(logging.INFO)
 
-    async def sendState(self):
+    def preMessageCommand(self):
+        self.mqtt.turnOnLight()
+
+    def postMessageCommand(self):
+        self.mqtt.turnOffLight()
+
+    async def sendState(self, prefix: str = ''):
+        self.preMessageCommand()
         self.logger.info_message('sending state')
-        paused_message = 'Job paused, probably the filament is empty.\n\n' if self.job.has_paused() else ''
-        message = '{paused_message}The job of {file} has reached {completion:.2f}% of completion.\nETA: {eta} hrs\n{link}'.format(
-            paused_message=paused_message,
+        message = '{prefix}The job of {file} has reached {completion:.2f}% of completion.\nETA: {eta} hrs\n{link}'.format(
+            prefix=prefix,
             file=self.job.file,
             completion=self.job.progress['completion'],
             eta=str(datetime.timedelta(
@@ -35,6 +43,7 @@ class Runtime():
         )
         image = self.octopi.get_image()
         await self.telegram.send_image(message, image)
+        self.postMessageCommand()
 
     async def main(self):
         load_dotenv()
@@ -46,7 +55,9 @@ class Runtime():
             time.sleep(sleep_time)
             self.job = self.octopi.get_status()
             if isinstance(self.job, Job):
-                if self.job.has_quarter_achieved() or self.job.has_paused():
+                if self.job.has_paused():
+                    await self.sendState(prefix='Job paused, probably the filament is empty.\n\n')
+                if self.job.has_quarter_achieved():
                     await self.sendState()
 
 
---
+++ mqtt_client.py
@@ -0,0 +1,81 @@
+import os
+import time
+import paho.mqtt.client as mqtt
+
+from logger import Logger
+
+
+class MQTT:
+    def __init__(self, debug: bool = False):
+        self.telegram_api_key = os.environ.get('TELEGRAM_API_KEY', 'token')
+        self.telegram_chat_id = os.environ.get('TELEGRAM_CHAT_ID', 'chat_id')
+        self.host = os.environ.get('MQTT_HOST', 'test.mosquitto.org')
+        try:
+            self.port = int(os.environ.get('MQTT_PORT', '1883'))
+        except:
+            raise EnvironmentError('MQTT_PORT must be a digit')
+        self.user = os.environ.get('MQTT_USER', 'user')
+        self.password = os.environ.get('MQTT_PASSWORD', 'password')
+        self.id = os.environ.get('MQTT_ID', '3d-bot')
+        self.lamp_topic = os.environ.get('MQTT_LAMP_TOPIC', '/command/topic')
+        self.lamp_on = os.environ.get('MQTT_LAMP_ON', 'on')
+        self.lamp_off = os.environ.get('MQTT_LAMP_OFF', 'off')
+        self.verify_ssl_certificate = os.environ.get(
+            'MQTT_SSL_VERIFICATION', False)
+        self.logger = Logger(debug=debug)
+
+        self.client = self.initClient()
+        self.connectClient()
+        self.client.on_connect = self.on_connect
+
+    def initClient(self):
+        self.logger.info_message(
+            'creating mqtt-client for {user} - {id}'.format(user=self.user, id=self.id))
+        client = mqtt.Client(
+            client_id=self.id,
+            clean_session=False
+        )
+        client.username_pw_set(
+            self.user,
+            self.password
+        )
+        return client
+
+    def connectClient(self):
+        self.logger.info_message('trying to connect to {host}:{port} ...'.format(
+            host=self.host, port=self.port))
+        self.logger.debug_message('usiing user {user}, id {id}, password {password}'.format(
+            user=self.user,
+            id=self.id,
+            password=self.password
+        ))
+        try:
+            result = self.client.connect(self.host, self.port)
+        except Exception as error:
+            self.logger.error_message('failed mqtt connection to {host}:{port} with result {result}'.format(
+                host=self.host, port=self.port, result=error))
+            return False
+        return True
+
+    def on_connect(self, userdata, flags, rc, result):
+        self.logger.info_message('successfull mqtt connection to {host}:{port}'.format(
+            host=self.host, port=self.port))
+        self.logger.debug_message('userdata: {userdata}\nflags: {flags}\nrc: {rc}\nresult: {result}'.format(
+            userdata=userdata,
+            flags=flags,
+            rc=rc,
+            result=result
+        ))
+
+    def turnOnLight(self):
+        self.publishMessage(self.lamp_topic, self.lamp_on)
+        time.sleep(5)
+
+    def turnOffLight(self):
+        self.publishMessage(self.lamp_topic, self.lamp_off)
+        time.sleep(5)
+
+    def publishMessage(self, topic: str, payload: str):
+        self.logger.info_message('sending mqtt payload {payload} on {topic}'.format(
+            payload=payload, topic=topic))
+        self.client.publish(topic=topic, payload=payload)
