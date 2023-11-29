51cc467155ca8942f09d10e8321ba983886d4a2a
klausklausen <test@test.test>
2023-11-29 09:55:03 +0100

    add fail-handling n telegram

---
+++ Makefile
@@ -0,0 +1,11 @@
+up:
+	docker-compose -f docker-compose.yml up -d
+
+down:
+	docker-compose -f docker-compose.yml down
+
+up-build:
+	docker-compose -f docker-compose.yml up -d --build
+
+dec_env:
+	sops -d workspace.env.encrypted > workspace.env
--- telegram_client.py
+++ telegram_client.py
@@ -9,24 +9,47 @@ from logger import Logger
 class Telegram:
     def __init__(self, debug: bool = False):
         # asyncio.ensure_future(self.send_message())
+        self.image_sent_failed_ctr = 0
+        self.message_sent_failed_ctr = 0
         self.telegram_api_key = os.environ.get('TELEGRAM_API_KEY', 'token')
         self.telegram_chat_id = os.environ.get('TELEGRAM_CHAT_ID', 'chat_id')
         self.logger = Logger(debug=debug)
         self.bot = ApplicationBuilder().token(self.telegram_api_key).build().bot
 
     async def send_message(self, message: str):
-        self.logger.info_message('sending message')
-        self.logger.debug_message('message: {message}'.format(message=message))
-        await self.bot.sendMessage(
-            chat_id=self.telegram_chat_id,
-            text=message
-        )
+        if self.message_sent_failed_ctr > 5:
+            self.logger.error_message(
+                'message sending failed more than 5 times')
+            return
+        try:
+            self.logger.info_message('sending message')
+            self.logger.debug_message(
+                'message: {message}'.format(message=message))
+            await self.bot.sendMessage(
+                chat_id=self.telegram_chat_id,
+                text=message
+            )
+        except Exception as e:
+            self.logger.error_message('message sending failed')
+            self.logger.error_message('error: {error}'.format(error=e))
+            self.message_sent_failed_ctr += 1
+        self.message_sent_failed_ctr = 0
 
     async def send_image(self, message: str, image: bytes):
-        self.logger.info_message('sending message')
-        self.logger.debug_message('message: {message}'.format(message=message))
-        await self.bot.send_photo(
-            chat_id=self.telegram_chat_id,
-            photo=image,
-            caption=message
-        )
+        if self.image_sent_failed_ctr > 5:
+            self.logger.error_message('image sending failed more than 5 times')
+            return
+        try:
+            self.logger.info_message('sending message')
+            self.logger.debug_message(
+                'message: {message}'.format(message=message))
+            await self.bot.send_photo(
+                chat_id=self.telegram_chat_id,
+                photo=image,
+                caption=message
+            )
+        except Exception as e:
+            self.logger.error_message('image sending failed')
+            self.logger.error_message('error: {error}'.format(error=e))
+            self.image_sent_failed_ctr += 1
+        self.image_sent_failed_ctr = 0
